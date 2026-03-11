0ba7252197a37a120c6fa3f46b132efd07683712
Surya <113543850+suryathomas@users.noreply.github.com>
2026-03-11 11:45:03 +0530

    Add basic addition functionality in addition.py

---
+++ addition.py
@@ -0,0 +1,11 @@
+# Addition of two numbers
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating sum
+sum = num1 + num2
+
+# Display result
+print("The sum is:", sum)
